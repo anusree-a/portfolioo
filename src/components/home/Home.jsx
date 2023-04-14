e137a1d40636b91a92367252b144a5163afbbd41
Anandhu <anandhuashok771@gmail.com>
2023-04-14 18:11:46 +0530

    hhi

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,7 +4,7 @@ import "./Home.css"
 function Home() {
   return (
     <div className='mainn'>
-        <h3 className='hi'>Hi,My name </h3>
+        <h3 className='hi'>Hi,My name is </h3>
         <h1 className='hei'>Anusree.A</h1>
         <h2>I am a full stack developer</h2>
         <p>I am a full stack developer specializing in building exceptional digital experiences.Currently iam focuse on building responsive full-stack web applications.</p>
